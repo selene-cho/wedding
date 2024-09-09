59e8714d1588dd8e26b2f83066aaf75895ee52ff
selene-cho <selene.cho29@gmail.com>
2024-09-10 00:16:36 +0900

    feat: db.json 데이터 통신, loading, error 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './App.module.scss';
+import FullScreenMessage from './components/shared/FullScreenMessage';
 
 const cx = classNames.bind(styles);
 
-function App() {
-  return <div className={cx('container')}>App</div>;
-}
+export default function App() {
+  const [wedding, setWedding] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-export default App;
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('http://localhost:4000/wedding1')
+      .then((response) => {
+        if (response.ok === false) {
+          throw new Error('청첩장 정보를 불러오지 못했습니다.');
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        setWedding(data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.log('에러', e);
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <FullScreenMessage type="loading" />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <FullScreenMessage type="error" />
+      </div>
+    );
+  }
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>;
+}
