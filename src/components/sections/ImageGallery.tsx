f6f26a9e7722c57d9e99a12f3c61964128e9b75e
selene-cho <selene.cho29@gmail.com>
2024-09-10 17:17:20 +0900

    feat: 갤러리 사진 보기 기능 추가, swiper 설치

--- src/components/sections/ImageGallery.tsx
+++ src/components/sections/ImageGallery.tsx
@@ -1,20 +1,48 @@
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './ImageGallery.module.scss';
 
 import Section from '@shared/Section';
+import ImageViewer from '../ImageViewer';
 
 const cx = classNames.bind(styles);
 
 export default function ImageGallery({ images }: { images: string[] }) {
+  const [selectedIdx, setSelectedIdx] = useState(-1);
+
+  const open = selectedIdx > -1;
+
+  const handleSelectedImage = (idx: number) => {
+    setSelectedIdx(idx);
+  };
+
+  const handleClose = () => {
+    setSelectedIdx(-1);
+  };
+
   return (
-    <Section title="갤러리">
-      <ul className={cx('wrap-images')}>
-        {images.map((src, idx) => (
-          <li key={idx} className={cx('wrap-image')}>
-            <img src={src} alt="사진첩 이미지" />
-          </li>
-        ))}
-      </ul>
-    </Section>
+    <>
+      <Section title="갤러리">
+        <ul className={cx('wrap-images')}>
+          {images.map((src, idx) => (
+            <li
+              key={idx}
+              className={cx('wrap-image')}
+              onClick={() => {
+                handleSelectedImage(idx);
+              }}
+            >
+              <img src={src} alt="사진첩 이미지" />
+            </li>
+          ))}
+        </ul>
+      </Section>
+      <ImageViewer
+        images={images}
+        open={open}
+        selectedIdx={selectedIdx}
+        onClose={handleClose}
+      />
+    </>
   );
 }
