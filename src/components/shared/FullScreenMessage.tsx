7f4d7fc955e359f848d46e640f3cc2a340f0d7cb
selene-cho <selene.cho29@gmail.com>
2024-09-11 21:30:29 +0900

    fix: Text 컴포넌트 생성, scss 수정

--- src/components/shared/FullScreenMessage.tsx
+++ src/components/shared/FullScreenMessage.tsx
@@ -24,12 +24,7 @@ export default function FullScreenMessage({ type }: FullScreenMessageProps) {
 
 function Heart() {
   return (
-    <svg
-      className={cx('icon-heart')}
-      version="1.1"
-      viewBox="0 0 512 512"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg className={cx('icon-heart')} version="1.1" viewBox="0 0 512 512">
       <g id="_x31_66_x2C__Heart_x2C__Love_x2C__Like_x2C__Twitter">
         <g>
           <path d="M365.4,59.628c60.56,0,109.6,49.03,109.6,109.47c0,109.47-109.6,171.8-219.06,281.271    C146.47,340.898,37,278.568,37,169.099c0-60.44,49.04-109.47,109.47-109.47c54.73,0,82.1,27.37,109.47,82.1    C283.3,86.999,310.67,59.628,365.4,59.628z" />
@@ -45,14 +40,11 @@ function Error() {
     <svg
       className={cx('icon-error')}
       fill="none"
-      height="24"
       stroke="currentColor"
       stroke-linecap="round"
       stroke-linejoin="round"
       stroke-width="2"
       viewBox="0 0 24 24"
-      width="24"
-      xmlns="http://www.w3.org/2000/svg"
     >
       <path d="M0 0h24v24H0z" fill="none" stroke="none" />
       <path d="M4 8v-2a2 2 0 0 1 2 -2h2" />
