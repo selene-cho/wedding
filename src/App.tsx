3948173f5a9b3989aa578920a78dd9ddf3d6895b
selene-cho <selene.cho29@gmail.com>
2024-09-11 21:33:35 +0900

    feat: 본문 구현(Intro, Invitation), utils.scss 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import FullScreenMessage from '@shared/FullScreenMessage';
 import Heading from '@components/sections/Heading';
 import Video from '@components/sections/Video';
 import ImageGallery from '@components/sections/ImageGallery';
+import Intro from '@/components/sections/Intro';
+import Invitation from '@components/sections/Invitation';
 
 const cx = classNames.bind(styles);
 
@@ -59,14 +61,28 @@ export default function App() {
     return null;
   }
 
-  const { date, galleryImages } = wedding;
+  const {
+    date,
+    galleryImages,
+    groom,
+    bride,
+    location,
+    message: { intro, invitation },
+  } = wedding;
 
   return (
     <div className={cx('container')}>
       <Heading date={date} />
       <Video />
+      <Intro
+        groomName={groom.name}
+        brideName={bride.name}
+        locationName={location.name}
+        date={date}
+        message={intro}
+      />
+      <Invitation message={invitation} />
       <ImageGallery images={galleryImages} />
-      {JSON.stringify(wedding)}
     </div>
   );
 }
