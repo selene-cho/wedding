7096b375ef9ba34530fde5f22afc5482c2f3e03a
selene-cho <selene.cho29@gmail.com>
2024-09-14 21:11:10 +0900

    feat: Modal Context, 참석 가능인원 체크 Modal 구현

---
+++ src/contexts/ModalContext.tsx
@@ -0,0 +1,57 @@
+import { createPortal } from 'react-dom';
+import { createContext, useContext, ComponentProps, useState } from 'react';
+
+import Modal from '@shared/Modal';
+
+type ModalProps = ComponentProps<typeof Modal>;
+type ModalOptions = Omit<ModalProps, 'open'>;
+
+interface ModalContextValue {
+  open: (options: ModalOptions) => void;
+  close: () => void;
+}
+
+const Context = createContext<ModalContextValue | undefined>(undefined);
+
+const defaultValues: ModalProps = {
+  open: false,
+  body: null,
+  onLeftButtonClick: () => {},
+  onRightButtonClick: () => {},
+};
+
+export function ModalContext({ children }: { children: React.ReactNode }) {
+  const [modalState, setModalState] = useState(defaultValues);
+
+  const $portal_root = document.getElementById('root-portal');
+
+  const open = (options: ModalOptions) => {
+    setModalState({ ...options, open: true });
+  };
+
+  const close = () => {
+    setModalState(defaultValues);
+  };
+
+  const values = {
+    open,
+    close,
+  };
+
+  return (
+    <Context.Provider value={values}>
+      {children}
+      {$portal_root && createPortal(<Modal {...modalState} />, $portal_root)}
+    </Context.Provider>
+  );
+}
+
+export function useModalContext() {
+  const values = useContext(Context);
+
+  if (!values) {
+    throw new Error('ModalContext 안에서 사용해주세요.');
+  }
+
+  return values;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { ModalContext } from '@contexts/ModalContext.tsx';
 import '@scss/global.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ModalContext>
+      <App />
+    </ModalContext>
   </StrictMode>,
 );
