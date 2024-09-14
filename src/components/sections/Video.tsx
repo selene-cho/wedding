a61a453d1557bcde3c853d70baea41cb4754a6ea
selene-cho <selene.cho29@gmail.com>
2024-09-14 22:05:22 +0900

    feat: 동영상 최적화

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -8,13 +8,9 @@ const cx = classNames.bind(styles);
 export default function Video() {
   return (
     <Section className={cx('container')}>
-      <video
-        autoPlay={true}
-        muted={true}
-        loop={true}
-        poster="/assets/poster.jpg"
-      >
-        <source src="/assets/main.mp4" type="video/mp4"></source>
+      <video autoPlay muted loop poster="/assets/images/poster.jpg">
+        <source src="/assets/videos/main.webm" type="video/webm" />
+        <source src="/assets/videos/main.mp4" type="video/mp4" />
       </video>
     </Section>
   );
