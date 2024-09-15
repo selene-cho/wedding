a2344fe1ecd947dcc15b907de9b94767396b518b
selene-cho <selene.cho29@gmail.com>
2024-09-15 22:34:11 +0900

    refactor: react-query로 서버 상태 관리

--- src/hooks/useWedding.ts
+++ src/hooks/useWedding.ts
@@ -1,35 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { Wedding } from '@models/wedding';
 import getWedding from '@/api/wedding';
 
 export default function useWedding() {
-  const [wedding, setWedding] = useState<Wedding | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-
-    getWedding()
-      .then((response) => {
+  const { data, isPending, error } = useQuery<Wedding>({
+    queryKey: ['wedding'],
+    queryFn: () =>
+      getWedding().then((response) => {
         if (response.ok === false) {
           throw new Error('청첩장 정보를 불러오지 못했습니다.');
         }
 
         return response.json();
-      })
-      .then((data) => {
-        setWedding(data);
-        setLoading(false);
-      })
-      .catch((e) => {
-        console.log('에러 발생', e);
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
+      }),
+  });
 
-  return { wedding, loading, error };
+  return { wedding: data, loading: isPending, error };
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import App from './App.tsx';
 import { ModalContext } from '@contexts/ModalContext.tsx';
 import '@scss/global.scss';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ModalContext>
-      <App />
-    </ModalContext>
+    <QueryClientProvider client={queryClient}>
+      <ModalContext>
+        <App />
+      </ModalContext>
+    </QueryClientProvider>
   </StrictMode>,
 );
