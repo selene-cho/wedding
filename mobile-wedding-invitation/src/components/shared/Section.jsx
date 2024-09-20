0d1665ff015548205ae4fc824c4e93736308bf70
selene-cho <selene.cho29@gmail.com>
2024-09-20 21:22:07 +0900

    Feat: Section 컴포넌트 구현

---
+++ mobile-wedding-invitation/src/components/shared/Section.jsx
@@ -0,0 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './Section.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function Section({ children, className, title, subtitle }) {
+  return (
+    <section className={cx('container', className)}>
+      {subtitle && <h3 className={cx('txt-subtitle')}>{subtitle}</h3>}
+      {title && <h1 className={cx('txt-title')}>{title}</h1>}
+      {children}
+    </section>
+  );
+}
