0db35cfd65146b90d14ad5a09fd10b9709bd055c
selene-cho <selene.cho29@gmail.com>
2024-09-20 21:20:43 +0900

    feat: 절대경로, 사진 추가

--- mobile-wedding-invitation/vite.config.js
+++ mobile-wedding-invitation/vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      {find: "@", replacement: "/src"},
-      {find: "@components", replacement: "/src/components"},
-      {find: "@scss", replacement: "/src/scss"}
+      { find: '@', replacement: '/src' },
+      { find: '@scss', replacement: '/src/scss' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@shared', replacement: '/src/components/shared' },
+      { find: '@sections', replacement: '/src/components/sections' },
+      { find: '@utils', replacement: '/src/utils' },
     ],
   },
-})
+});
