15164fe97885517cb35303557439b9c7f52c978d
selene-cho <selene.cho29@gmail.com>
2024-09-23 23:56:49 +0900

    feat: Modal 컴포넌트 구현

---
+++ mobile-wedding-invitation/src/components/shared/Modal.jsx
@@ -0,0 +1,19 @@
+import classNames from 'classnames/bind';
+
+import Dimmed from '@shared/Dimmed';
+import IconClose from '@icons/IconClose';
+
+import styles from './Modal.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function Modal({ children, isOpened = false, handleClose }) {
+  if (!isOpened) return;
+
+  return (
+    <Dimmed>
+      <IconClose handleClose={handleClose} />
+      <div className={cx('container')}>{children}</div>
+    </Dimmed>
+  );
+}
