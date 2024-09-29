b0ca0f25680619b2298617887b8d34a4b67c565a
selene-cho <selene.cho29@gmail.com>
2024-09-29 17:04:03 +0900

    fix: 카카오 공유하기 이미지 변경

--- mobile-wedding-invitation/src/components/sections/Share.jsx
+++ mobile-wedding-invitation/src/components/sections/Share.jsx
@@ -39,7 +39,7 @@ export default function Share({ groomName, brideName, date, location }) {
         imageUrl: `${generateAssetsUrl({
           small: true,
           format: 'jpg',
-          filename: 'wedding_11',
+          filename: 'wedding_01',
         })}`,
         link: {
           mobileWebUrl: window.location.origin,
